d6a5b81d16c2a0c72baeef48ddcb5ca4c0c21461
tonis <tonis.teder@gmail.com>
2024-02-09 22:05:47 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab teda nimepidi
+
+
+Console.WriteLine("enter your name");
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+Console.WriteLine($"Hello, {userName} !");
